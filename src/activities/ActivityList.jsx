7970670bd57670b25c2258c136175c614ef40e11
Aniello-Bacarella <aniello.bacarella@gmail.com>
2025-11-30 22:58:43 -0500

    added delete button to activity list

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -2,7 +2,12 @@ export default function ActivityList({ activities }) {
   return (
     <ul>
       {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}</li>
+        <li key={activity.id}>
+          {activity.name}
+          {token && (
+            <button onClick={() => onDelete(activity.id)}>Delete</button>
+          )}
+        </li>
       ))}
     </ul>
   );
